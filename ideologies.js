0b7cd5b4e72017167c6c000699ff1b6bb5a27712
eurodengist <mikolaus54@proton.me>
2025-09-30 05:26:07 +0000

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,421 +1,376 @@
 ideologies = [
     {
-        "name": "Christian Theocrat",
-        "stats": {
-            "cath": 50,
-            "prot": 50,
-            "orth": 50,
-            "jews": 10,
-            "islm": 10,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 0,
-            "satn": 0,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Catholic Theocrat",
-        "stats": {
-            "cath": 100,
-            "prot": 20,
-            "orth": 20,
-            "jews": 15,
-            "islm": 10,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 0,
-            "satn": 0,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Protestant Theocrat",
-        "stats": {
-            "cath": 10,
-            "prot": 100,
-            "orth": 10,
-            "jews": 30,
-            "islm": 10,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 0,
-            "satn": 0,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Eastern Orthodox Theocrat",
-        "stats": {
-            "cath": 30,
-            "prot": 20,
-            "orth": 100,
-            "jews": 10,
-            "islm": 10,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 0,
-            "satn": 0,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Islamic Theocrat",
-        "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 10,
-            "islm": 100,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 0,
-            "satn": 0,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Jewish Theocrat",
-        "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 100,
-            "islm": 0,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 0,
-            "satn": 0,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Buddhist Theocrat",
-        "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 10,
-            "islm": 10,
-            "budd": 100,
-            "hind": 60,
-            "pagn": 50,
-            "satn": 30,
-            "athe": 80
-        }
-    },
-	    {
-        "name": "Pagan Theocrat",
-        "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 10,
-            "islm": 10,
-            "budd": 20,
-            "hind": 30,
-            "pagn": 100,
-            "satn": 40,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Satanic Theocrat",
-        "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 10,
-            "islm": 10,
-            "budd": 20,
-            "hind": 20,
-            "pagn": 30,
-            "satn": 100,
-            "athe": 20
-        }
-    },
-	    {
-        "name": "Secularist",
-        "stats": {
-            "cath": 40,
-            "prot": 40,
-            "orth": 40,
-            "jews": 40,
-            "islm": 40,
-            "budd": 40,
-            "hind": 40,
-            "pagn": 40,
-            "satn": 40,
-            "athe": 30
-        }
-    },
-	    {
-        "name": "Atheist Theocrat",
-        "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 10,
-            "islm": 10,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 10,
-            "satn": 20,
-            "athe": 10
-        }
-    },
-	    {
-        "name": "Evangelical Protestant Theocrat",
-        "stats": {
-            "cath": 20,
-            "prot": 100,
-            "orth": 20,
-            "jews": 50,
-            "islm": 10,
-            "budd": 10,
-            "hind": 10,
-            "pagn": 10,
-            "satn": 10,
-            "athe": 100
-        }
-    },
-	    {
-        "name": "Modern Western Buddhist",
-        "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 10,
-            "islm": 10,
-            "budd": 60,
-            "hind": 50,
-            "pagn": 20,
-            "satn": 10,
-            "athe": 50
-        }
-    },
-	    {
-        "name": "Positive Christian",
-        "stats": {
-            "cath": 20,
-            "prot": 50,
-            "orth": 20,
-            "jews": 0,
-            "islm": 10,
-            "budd": 20,
-            "hind": 20,
-            "pagn": 10,
-            "satn": 20,
-            "athe": 30
-        }
-    },
-	    {
-        "name": "Moderate Jew",
-        "stats": {
-            "cath": 20,
-            "prot": 20,
-            "orth": 20,
-            "jews": 50,
-            "islm": 20,
-            "budd": 35,
-            "hind": 35,
-            "pagn": 30,
-            "satn": 20,
-            "athe": 50
-        }
-    },
-	    {
-        "name": "Agnostic",
-        "stats": {
-            "cath": 40,
-            "prot": 40,
-            "orth": 40,
-            "jews": 40,
-            "islm": 40,
-            "budd": 40,
-            "hind": 40,
-            "pagn": 40,
-            "satn": 40,
-            "athe": 50
-        }
-    },
-	    {
-        "name": "Moderate Catholic",
-        "stats": {
-            "cath": 60,
-            "prot": 30,
-            "orth": 30,
-            "jews": 30,
-            "islm": 30,
-            "budd": 30,
-            "pagn": 30,
-            "satn": 20,
-            "athe": 60
-        }
-    },
-	    {
-        "name": "Moderate Protestant",
-        "stats": {
-            "cath": 30,
-            "prot": 60,
-            "orth": 30,
-            "jews": 30,
-            "islm": 30,
-            "budd": 30,
-            "hind": 30,
-            "pagn": 30,
-            "satn": 20,
-            "athe": 60
-        }
-    },
-	    {
-        "name": "Eastern Orthodox Moderate",
-        "stats": {
-            "cath": 30,
-            "prot": 30,
-            "orth": 60,
-            "jews": 30,
-            "islm": 30,
-            "budd": 30,
-            "hind": 30,
-            "pagn": 30,
-            "satn": 20,
-            "athe": 60
-        }
-    },
-	    {
-        "name": "Moderate Muslim",
-        "stats": {
-            "cath": 30,
-            "prot": 30,
-            "orth": 30,
-            "jews": 30,
-            "islm": 60,
-            "budd": 30,
-            "hind": 30,
-            "pagn": 30,
-            "satn": 30,
-            "athe": 60
-        }
-    },
-	    {
         "name": "Universalist Christian",
         "stats": {
-            "cath": 40,
-            "prot": 60,
-            "orth": 40,
-            "jews": 40,
-            "islm": 40,
-            "budd": 40,
-            "hind": 40,
-            "pagn": 40,
-            "satn": 20,
-            "athe": 70
+            "a": 90,
+            "b": 90,
+            "c": 90,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
         }
     },
-	    {
-        "name": "Secular Buddhist",
+	{
+        "name": "Catholic",
         "stats": {
-            "cath": 30,
-            "prot": 30,
-            "orth": 30,
-            "jews": 30,
-            "islm": 30,
-            "budd": 60,
-            "hind": 60,
-            "pagn": 40,
-            "satn": 30,
-            "athe": 60
+            "a": 100,
+            "b": 30,
+            "c": 30,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
         }
     },
-	    {
+	{
+        "name": "Protestant",
+        "stats": {
+            "a": 30,
+            "b": 100,
+            "c": 30,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
+        }
+    },
+	{
+        "name": "Eastern Orthodox",
+        "stats": {
+            "a": 30,
+            "b": 30,
+            "c": 100,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
+        }
+    },
+	{
+        "name": "Sunni",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 100,
+            "f": 30,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
+        }
+    },
+    {
+        "name": "Shia",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 30,
+            "f": 100,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
+        }
+    },
+    {
+        "name": "Universalist Muslim",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 100,
+            "f": 100,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
+        }
+    },
+	{
+        "name": "Jew",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 100,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 100
+        }
+    },
+	{
+        "name": "Mahayana Buddhist",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 100,
+            "g": 55,
+            "h": 40,
+            "i": 90
+        }
+    },
+	{
+        "name": "Pagan Theocrat",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 30,
+            "h": 100,
+            "i": 100
+        }
+    },
+	{
+        "name": "Secularist",
+        "stats": {
+            "a": 40,
+            "b": 40,
+            "c": 40,
+            "d": 40,
+            "e": 40,
+            "f": 40,
+            "x": 40,
+            "g": 40,
+            "h": 40,
+            "i": 30
+        }
+    },
+	{
+        "name": "Atheist",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 0
+        }
+    },
+	{
+        "name": "Theravada Buddhist",
+        "stats": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 80,
+            "g": 0,
+            "h": 0,
+            "i": 30
+        }
+    },
+	{
+        "name": "Moderate Jew",
+        "stats": {
+            "a": 20,
+            "b": 20,
+            "c": 20,
+            "d": 60,
+            "e": 20,
+            "f": 20,
+            "x": 20,
+            "g": 20,
+            "h": 20,
+            "i": 60
+        }
+    },
+	{
+        "name": "Agnostic",
+        "stats": {
+            "a": 40,
+            "b": 40,
+            "c": 40,
+            "d": 40,
+            "e": 40,
+            "f": 40,
+            "x": 40,
+            "g": 40,
+            "h": 40,
+            "i": 50
+        }
+    },
+	{
+        "name": "Moderate Catholic",
+        "stats": {
+            "a": 60,
+            "b": 30,
+            "c": 30,
+            "d": 30,
+            "e": 30,
+            "f": 30,
+            "g": 30,
+            "h": 20,
+            "i": 60
+        }
+    },
+	{
+        "name": "Moderate Protestant",
+        "stats": {
+            "a": 30,
+            "b": 60,
+            "c": 30,
+            "d": 30,
+            "e": 30,
+            "f": 30,
+            "x": 30,
+            "g": 30,
+            "h": 20,
+            "i": 60
+        }
+    },
+	{
+        "name": "Moderate Orthodox",
+        "stats": {
+            "a": 30,
+            "b": 30,
+            "c": 60,
+            "d": 30,
+            "e": 30,
+            "f": 30,
+            "x": 30,
+            "g": 30,
+            "h": 20,
+            "i": 60
+        }
+    },
+	{
+        "name": "Moderate Muslim",
+        "stats": {
+            "a": 30,
+            "b": 30,
+            "c": 30,
+            "d": 30,
+            "e": 60,
+            "f": 60,
+            "x": 30,
+            "g": 30,
+            "h": 30,
+            "i": 60
+        }
+    },
+    {
+        "name": "Moderate Sunni",
+        "stats": {
+            "a": 30,
+            "b": 30,
+            "c": 30,
+            "d": 30,
+            "e": 60,
+            "f": 30,
+            "x": 30,
+            "g": 30,
+            "h": 30,
+            "i": 60
+        }
+    },
+    {
+        "name": "Moderate Shia",
+        "stats": {
+            "a": 30,
+            "b": 30,
+            "c": 30,
+            "d": 30,
+            "e": 30,
+            "f": 60,
+            "x": 30,
+            "g": 30,
+            "h": 30,
+            "i": 60
+        }
+    },
+	{
         "name": "Secular Pagan",
         "stats": {
-            "cath": 30,
-            "prot": 30,
-            "orth": 30,
-            "jews": 30,
-            "islm": 30,
-            "budd": 40,
-            "hind": 40,
-            "pagn": 60,
-            "satn": 40,
-            "athe": 60
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 60,
+            "i": 40
         }
     },
-	    {
-        "name": "Secular Satanist",
-        "stats": {
-            "cath": 30,
-            "prot": 30,
-            "orth": 30,
-            "jews": 30,
-            "islm": 30,
-            "budd": 30,
-            "hind": 30,
-            "pagn": 40,
-            "satn": 60,
-            "athe": 60
-        }
-    },
-	    {
+	{
         "name": "Pantheist",
         "stats": {
-            "cath": 60,
-            "prot": 60,
-            "orth": 60,
-            "jews": 60,
-            "islm": 60,
-            "budd": 60,
-            "hind": 60,
-            "pagn": 60,
-            "satn": 60,
-            "athe": 60
+            "a": 60,
+            "b": 60,
+            "c": 60,
+            "d": 60,
+            "e": 60,
+            "f": 60,
+            "x": 60,
+            "g": 60,
+            "h": 60,
+            "i": 60
         }
     },
-	    {
-        "name": "Secular Atheist",
-        "stats": {
-            "cath": 30,
-            "prot": 30,
-            "orth": 30,
-            "jews": 30,
-            "islm": 30,
-            "budd": 30,
-            "hind": 30,
-            "pagn": 30,
-            "satn": 30,
-            "athe": 30
-        }
-    },
-	    {
+	{
         "name": "Secular Hindu",
         "stats": {
-            "cath": 30,
-            "prot": 30,
-            "orth": 30,
-            "jews": 30,
-            "islm": 30,
-            "budd": 30,
-            "hind": 65,
-            "pagn": 40,
-            "satn": 30,
-            "athe": 65
+            "a": 30,
+            "b": 30,
+            "c": 30,
+            "d": 30,
+            "e": 30,
+            "f": 30,
+            "x": 30,
+            "g": 65,
+            "h": 30,
+            "i": 65
         }
     },
-	    {
-        "name": "Hindu Theocrat",
+	{
+        "name": "Hindu",
         "stats": {
-            "cath": 10,
-            "prot": 10,
-            "orth": 10,
-            "jews": 10,
-            "islm": 10,
-            "budd": 70,
-            "hind": 90,
-            "pagn": 60,
-            "satn": 10,
-            "athe": 100
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 40,
+            "g": 100,
+            "h": 10,
+            "i": 100
         }
     }
 ];
